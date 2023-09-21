a2cd25ba8aff43174f3a281318603480e0991d0c
Tim Smyth <tim.smyth@fiware.org>
2023-09-21 11:02:32 +0200

    api update

--- src/main/java/org/fiware/iam/til/TrustedIssuerConfigProvider.java
+++ src/main/java/org/fiware/iam/til/TrustedIssuerConfigProvider.java
@@ -5,7 +5,11 @@ import jakarta.inject.Singleton;
 import lombok.RequiredArgsConstructor;
 import org.fiware.iam.til.model.ClaimVO;
 import org.fiware.iam.til.model.CredentialsVO;
+import org.fiware.iam.til.model.TimeRangeVO;
 
+import java.time.Duration;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.Arrays;
 import java.util.List;
 
@@ -15,13 +19,16 @@ public class TrustedIssuerConfigProvider {
 
     private final TrustedIssuerConfig trustedIssuerConfig;
 
-    public CredentialsVO createCredentialConfigForTargetService(){
+    public CredentialsVO createCredentialConfigForTargetService() {
         List<ClaimVO> claims = trustedIssuerConfig
                 .getClaims()
                 .stream()
                 .map(c -> new ClaimVO().name(c.getTarget()).allowedValues(Arrays.asList(c.getRoles().toArray())))
                 .toList();
 
-        return new CredentialsVO().credentialsType(trustedIssuerConfig.getCredentialType()).claims(claims);
+        return new CredentialsVO()
+                .credentialsType(trustedIssuerConfig.getCredentialType())
+                .claims(claims)
+                .validFor(new TimeRangeVO().from(Instant.now()).to(Instant.now().plus(Duration.ofDays(365))));
     }
 }
